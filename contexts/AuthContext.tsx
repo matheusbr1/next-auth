59c1991040b7dc6b75238bb9c5a78e74fac41f0e
Matheus Baron <matheusbaron10@gmail.com>
2021-10-04 22:51:43 -0300

    Feat: refresh token and token added to SSR

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import Router from 'next/router'
-import api from "../services/api";
+import { api } from "../services/apiClient"
 import { setCookie, parseCookies, destroyCookie } from 'nookies'
 
 type SignInCredentials = {
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,10 @@
+import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
-import api from "../services/api"
+import { setupAPIClient } from "../services/api"
+import { api } from "../services/apiClient"
+import { AuthTokenError } from "../services/errors/AuthTokenError"
+import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard () {
   const { user } = useContext(AuthContext)
@@ -18,3 +22,14 @@ export default function Dashboard () {
     </>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx as any)
+  
+  const response = await apiClient.get('me')
+  console.log(response.data)
+
+  return {
+    props: {}
+  }
+})
